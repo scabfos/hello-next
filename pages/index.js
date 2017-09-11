387ede90c8e8aed94cb32f450a00d9dda22132bb
Santiago Cabrera <scabfos@gmail.com>
2017-09-11 11:11:56 +0200

    url masking

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const PostLink = (props) => (
     <li>
-        <Link href={`/post?title=${props.title}`}>
+        <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
             <a>{props.title}</a>
         </Link>
     </li>
@@ -13,9 +13,9 @@ const Index = () => (
     <Layout>
         <h1>My Blog</h1>
         <ul>
-            <PostLink title="Hello Next.js" />
-            <PostLink title="Learn Next.js is awesome!" />
-            <PostLink title="Da real Next" />
+            <PostLink id="hello-nextjs" title="Hello Next.js" />
+            <PostLink id="learn-nextjs" title="Learn Next.js is awesome!" />
+            <PostLink id="da-real-next" title="Da real Next" />
         </ul>
     </Layout>
 );
